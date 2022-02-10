cf61e5e18fc0361ed74ee86c591e4b61eb3138fa
Rithanya <78862080+Rithanya20@users.noreply.github.com>
2022-02-10 14:09:13 +0530

    Create check if str2 is rotation of str1.cpp

---
+++ check if str2 is rotation of str1.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s1, s2;
+    cin>>s1;
+    cin>>s2;
+    string s = s1+s1;
+    if(s.find(s2) != string::npos){
+      cout<<"YES";
+     
+    }
+    else{
+        cout<<"NO";
+    }
+    
+    return 0;
+}
+vicky
+cvkyi
+NO
+helloworld
+ldhellowor
+YES
